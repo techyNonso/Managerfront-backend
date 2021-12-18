f2de82723773cd89058032569a5dab5feef87a29
techyNonso <williamikeji@gmail.com>
2021-12-18 01:09:17 -0800

    setup

--- managerfront/settings.py
+++ managerfront/settings.py
@@ -10,9 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-import django
-django.setup()
-
 from pathlib import Path
 import os
 from decouple import config
