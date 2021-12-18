0e432603746651d53ee902bd806c76bc926955ee
techyNonso <williamikeji@gmail.com>
2021-12-18 01:30:36 -0800

    again

--- managerfront/asgi.py
+++ managerfront/asgi.py
@@ -18,15 +18,17 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 import django
+django.setup()
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 
 from . import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "managerfront.settings")
-django.setup()
+
 
 application = ProtocolTypeRouter(
     {
