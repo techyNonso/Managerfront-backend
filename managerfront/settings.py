0ab98031c1aad392a05e7b8da00b38916837bccf
techyNonso <williamikeji@gmail.com>
2021-12-18 09:01:29 -0800

    static

--- managerfront/settings.py
+++ managerfront/settings.py
@@ -160,12 +160,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_URL='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "dist")]
+STATIC_URL = "/static/"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
